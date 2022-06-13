9345b53d41723b2b6d0220c2ea62fc1c71265ed4
Dani <danisuesmith@gmail.com>
2022-06-13 12:43:23 -0700

    commit tree 4c49a1e8af7509207c4e39f3ae8edfb8bbdf9f5e

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // 1 step
+  const seenNumbers = {};
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    // n steps
+    if (seenNumbers[complement]) return true;
+    // n steps
+    seenNumbers[number] = true;
+  }
+  // 1 step
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Time complexity: O(n)
+  Space complexity: O(n)
 */
 
 /* 
-  Add your pseudocode here
+  create an object to keep track of all the numbers we've seen
+iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  check if any of the keys in our object is the complement to the current number
+    if so, return true
+  save the current number as the key on our object so we can check it later against other numbers
+if we reach the end of the array, return false
 */
 
 /*
-  Add written explanation of your solution here
+ iterate through array to find complement so that it equals the target 
 */
 
 // You can run `node index.js` to view these console logs
